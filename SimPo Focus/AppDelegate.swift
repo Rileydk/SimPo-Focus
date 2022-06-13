f7640e309e5853a1bf08d43bf8904674961045dc
Riley <dakimi07@gmail.com>
2022-06-13 15:48:47 +0800

    deal with error: MGIsDeviceOneOfType is not supported on this platform

--- SimPo Focus/AppDelegate.swift
+++ SimPo Focus/AppDelegate.swift
@@ -17,7 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
       // iOS 13的設定會在SceneDelegate被執行
     } else {
       let window = UIWindow(frame: UIScreen.main.bounds)
-      // self.window = window
       window.rootViewController = MainViewController()
       window.makeKeyAndVisible()
     }
@@ -34,7 +33,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func applicationDidBecomeActive(_ application: UIApplication) {}
   
-  // func applicationWillTerminate(_ application: UIApplication) {}
+  func applicationWillTerminate(_ application: UIApplication) {}
 
   // MARK: - UISceneSession Lifecycle
 
