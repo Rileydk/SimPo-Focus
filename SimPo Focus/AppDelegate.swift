5b5443d8753017eae035a3a435f421bf34555cbf
Riley <dakimi07@gmail.com>
2022-06-13 14:39:01 +0800

    ask compiler if iOS version is lower than 13, then start SimPo through AppDelegate

--- SimPo Focus/AppDelegate.swift
+++ SimPo Focus/AppDelegate.swift
@@ -12,17 +12,40 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
+    
+    if #available(iOS 13.0, *) {
+      // iOS 13的設定會在SceneDelegate被執行
+    } else {
+      let window = UIWindow(frame: UIScreen.main.bounds)
+      // self.window = window
+      window.rootViewController = MainViewController()
+      window.makeKeyAndVisible()
+    }
+    
     return true
   }
+  
+  //MARK: - App Lifecycle for version lower than iOS 13
+  func applicationWillResignActive(_ application: UIApplication) {}
 
-  // MARK: UISceneSession Lifecycle
+  func applicationDidEnterBackground(_ application: UIApplication) {}
 
+  func applicationWillEnterForeground(_ application: UIApplication) {}
+
+  func applicationDidBecomeActive(_ application: UIApplication) {}
+  
+  // func applicationWillTerminate(_ application: UIApplication) {}
+
+  // MARK: - UISceneSession Lifecycle
+
+  @available(iOS 13.0, *)
   func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
     // Called when a new scene session is being created.
     // Use this method to select a configuration to create the new scene with.
     return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
   }
 
+  @available(iOS 13.0, *)
   func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
     // Called when the user discards a scene session.
     // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
