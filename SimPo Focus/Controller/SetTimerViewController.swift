76024d23a98eb9de1f6e9c40d16c5fe4f4263b69
Riley <dakimi07@gmail.com>
2022-06-14 12:00:19 +0800

    add taskLabel

--- SimPo Focus/Controller/SetTimerViewController.swift
+++ SimPo Focus/Controller/SetTimerViewController.swift
@@ -13,6 +13,7 @@ class SetTimerViewController: UIViewController {
   var task = ""
   
   var stateLabel = UILabel()
+  var taskLabel = UILabel()
   
   let steelBlue = UIColor(red: 63/255, green: 114/255, blue: 175/255, alpha: 1)
   let culturedWhite = UIColor(red: 249/255, green: 247/255, blue: 247/255, alpha: 1)
@@ -23,6 +24,7 @@ class SetTimerViewController: UIViewController {
     // View
     self.view.backgroundColor = culturedWhite
     configureStateLabel()
+    configureTaskLabel()
   }
     
   func configureStateLabel() {
@@ -39,6 +41,22 @@ class SetTimerViewController: UIViewController {
     NSLayoutConstraint.activate([
       stateLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
       stateLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+    ])
+  }
+  
+  func configureTaskLabel() {
+    view.addSubview(taskLabel)
+    
+    taskLabel.text = task
+    taskLabel.font = .systemFont(ofSize: 28)
+    taskLabel.textColor = steelBlue
+    taskLabel.textAlignment = .center
+    
+    taskLabel.translatesAutoresizingMaskIntoConstraints = false
+    
+    NSLayoutConstraint.activate([
+      taskLabel.topAnchor.constraint(equalTo: stateLabel.bottomAnchor, constant: 20),
+      taskLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
     ])
   }
 
