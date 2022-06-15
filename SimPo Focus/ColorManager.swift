5560cda690377a401e442a479e711206a1c37e1b
Riley <dakimi07@gmail.com>
2022-06-15 16:50:58 +0800

    refactor color manager

---
+++ SimPo Focus/ColorManager.swift
@@ -0,0 +1,20 @@
+//
+//  ColorManager.swift
+//  SimPo Focus
+//
+//  Created by Riley Lai on 2022/6/15.
+//
+
+import UIKit
+
+#warning("delete this line after finish all view settings")
+let testColor = UIColor(red: 240/255, green: 128/255, blue: 128/255, alpha: 1)
+let steelBlue = UIColor(red: 63/255, green: 114/255, blue: 175/255, alpha: 1)
+let opaqueSteelBlue = UIColor(red: 63/255, green: 114/255, blue: 175/255, alpha: 0.6)
+let darkCerulean = UIColor(red: 15/255, green: 76/255, blue: 117/255, alpha: 1)
+let opaqueDarkCerulean = UIColor(red: 15/255, green: 76/255, blue: 117/255, alpha: 0.6)
+let spaceCadet = UIColor(red: 17/255, green: 45/255, blue: 78/255, alpha: 1)
+let culturedWhite = UIColor(red: 249/255, green: 247/255, blue: 247/255, alpha: 1)
+let gainsboro = UIColor(red: 221/255, green: 221/255, blue: 221/255, alpha: 1)
+let coolGray = UIColor(red: 141/255, green: 147/255, blue: 171/255, alpha: 1)
+let romainSilver = UIColor(red: 126/255, green: 138/255, blue: 151/255, alpha: 1)
--- SimPo Focus/Controller/MainViewController.swift
+++ SimPo Focus/Controller/MainViewController.swift
@@ -9,13 +9,6 @@
 
 import UIKit
 
-// Colors
-#warning("delete this line after finish all view settings")
-let testColor = UIColor(red: 240/255, green: 128/255, blue: 128/255, alpha: 1)
-let steelBlue = UIColor(red: 63/255, green: 114/255, blue: 175/255, alpha: 1)
-let opaqueSteelBlue = UIColor(red: 63/255, green: 114/255, blue: 175/255, alpha: 0.6)
-let culturedWhite = UIColor(red: 249/255, green: 247/255, blue: 247/255, alpha: 1)
-
 class MainViewController: UIViewController {
   
   //TODO: - try changing to UITextView to fulfill mutiple lines task title
@@ -182,7 +175,6 @@ class MainViewController: UIViewController {
   @objc func changeButtonBackgroundColorWhenTouchUpInside(sender: UIButton) {
     sender.backgroundColor = gainsboro
     sender.layer.shadowColor = spaceCadet.cgColor
-    
   }
   
   @objc func changeButtonBackgroundColorWhenReleased(sender: UIButton) {
